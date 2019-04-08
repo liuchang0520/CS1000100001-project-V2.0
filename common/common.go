88af6bbf40dce267edb2b4c67c9e8e29032db159
liuchang0520 <liuchang0520@outlook.com>
2019-04-08 12:20:07 -0400

    rename and redir

---
+++ common/common.go
@@ -0,0 +1,19 @@
+package commmon
+
+const (
+	MASTER_PORT = "5450"
+	MAX_WORKER = 10
+)
+
+type WorkArgs struct {
+	Task string
+}
+type WorkerRes struct {
+
+}
+type RegisterArgs struct {
+	Port string
+}
+type MasterRes struct {
+	
+}
---
+++ worker/worker.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"net/rpc"
+	"net/http"
+	"common"
+)
+
+type Worker struct { //Worker Struct
+	shutDownChan chan bool
+}
+//implement worker's rpc api
+//this rpc api is used to call worker to do a specific map/reduce task
+func (worker *Worker) Work(args *WorkArgs, res *WorkerRes) error {
+	return nil
+}
+
+
+func workerInit(port string) {
+	worker := new(Worker)
+
+	if err := rpc.Register(worker); err != nil {
+		log.Fatal("worker rpc setup failed", err)
+	}
+	rpc.HandleHTTP()
+	// fmt.Println("http handler started")
+	// fmt.Println("listener started")
+
+	workerClient, err := rpc.DialHTTP("tcp", "localhost:" + MASTER_PORT)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("client created")
+
+	//register
+	if err = workerClient.Call("Master.RegisterWorker", &RegisterArgs{Port: port}, &MasterRes{}); err != nil {
+		log.Fatal(err)
+	}
+
+	////TODO: keep listener in worker struct
+	go func() {if err := http.ListenAndServe("localhost:" + port, nil); err != nil {
+			log.Fatal("Failed to start worker process", err)
+	}}()
+	<- worker.shutDownChan
+}
+
+func main() {
+	//args[1] indicates the port running worker process
+	if len(os.Args) != 2 {
+		log.Fatal("pls specify the worker process port number", " args: ", os.Args)
+	}
+
+	port := os.Args[1]
+	fmt.Printf("worker will run at port: %s\n", port)
+
+	workerInit(port)
+}
