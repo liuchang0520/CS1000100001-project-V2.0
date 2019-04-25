f6e1f61edaf85430ac8477efbdbde5b3984505c6
liuchang0520 <liuchang0520@outlook.com>
2019-04-25 11:46:42 -0400

    skeleton for work api

--- src/common/common.go
+++ src/common/common.go
@@ -8,10 +8,18 @@ const (
 	MAX_WORKER = 10
 
 	MAX_TEMP = 10 //maximum number of task temp before announcing failure
+
+	outputDir := ""
 )
 
 type WorkArgs struct {
 	Task string
+	Stage string //map or reduce
+
+	//for map, it is an input file; for reducer, it is a directory containing the intermediate files
+	inputFile string 
+
+
 }
 type WorkerRes struct {
 
@@ -22,3 +30,5 @@ type RegisterArgs struct {
 type MasterRes struct {
 	
 }
+
+//TODO: map[functionName]function e.g. "wordCount": func wordCount()....
--- src/worker/worker_api.go
+++ src/worker/worker_api.go
@@ -15,11 +15,29 @@ import (
 func (worker *Worker) Work(args *c.WorkArgs, res *c.WorkerRes) error {
 	/*
 	map:
-	put all intermediate files for a reducer into a folder
+	0) read corresponding input file
+	1) transform the file content to k:v pairs
+		input: 
+		k: input file name
+		v: content
+
+	2) for each k, do hash(k) % r, to get which intermediate file dir to put to
+	   put all intermediate files for a reducer into a folder
 	*/
 	
 	/*
+	reduce:
+
+	1 output file per reducer
+	0) read the ith directory, in which intermediate files for this reducer is stored
+	1) transform to k: v0,v1,v2....combiner
+	2) call reduce function for each k, v
+		input: 
+		k: the key from intermediate files
+		v: the aggregated value list
+	3) for each output of reduce function, put it into the output file
 	*/
+
 	
 
 
